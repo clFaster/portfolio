7094dbaeba892731ccebd1f07d76847a268085e2
Moritz Reis <me@moritzreis.dev>
2025-05-23 20:10:27 +0200

    feat: implement NotFound page with styled components

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,25 @@
 import { Title } from "../components/styled/PageStyled.ts";
+import { useNavigate } from "react-router-dom";
+import { NotFoundButton, NotFoundContainer, NotFoundText } from "../components/styled/NotFoundStyled.ts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceSurprise, faHome } from "@fortawesome/free-solid-svg-icons";
 
 function NotFound() {
+  const navigate = useNavigate();
   return (
-    <Title>Not Found</Title>
+    <NotFoundContainer>
+      <Title>
+        404 - Not Found <FontAwesomeIcon icon={faFaceSurprise} />
+      </Title>
+      <NotFoundText>
+        Whoops! There’s nothing here.<br />
+        Maybe try heading home?
+      </NotFoundText>
+      <NotFoundButton onClick={() => navigate("/")}>
+        <FontAwesomeIcon icon={faHome} />
+        Go Home
+      </NotFoundButton>
+    </NotFoundContainer>
   );
 }
 
